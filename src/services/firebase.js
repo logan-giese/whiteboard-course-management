989383b372113449cc83215656709e40051e156e
sagarbadu <sagar.badu29@gmail.com>
2021-07-06 17:48:07 -0500

    Login int
    Login integration version 2

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,19 +1,22 @@
-// Import Firebase dependencies
-import firebase from "firebase/app";
+import firebase from 'firebase';
 import "firebase/firestore";
-//import "firebase/auth";
 
-// Firebase configuration (required to connect)
-const firebaseConfig = {
-  apiKey: "AIzaSyDwpAGRWo_GikLeCdhRPbxJBm1g-uwIq_M",
-  authDomain: "whiteboard-course-management.firebaseapp.com",
-  projectId: "whiteboard-course-management",
-  storageBucket: "whiteboard-course-management.appspot.com",
-  messagingSenderId: "215690108847",
-  appId: "1:215690108847:web:bcfbccc0fd574d4c05195a"
-};
+  // Web app's Firebase configuration
+  var firebaseConfig = {
+    
+    apiKey: "AIzaSyDwpAGRWo_GikLeCdhRPbxJBm1g-uwIq_M",
+    authDomain: "whiteboard-course-management.firebaseapp.com",
+    projectId: "whiteboard-course-management",
+    storageBucket: "whiteboard-course-management.appspot.com",
+    messagingSenderId: "215690108847",
+    appId: "1:215690108847:web:bcfbccc0fd574d4c05195a"
+    
+  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+
+  // Initialize Firebase
+  const fire = firebase.initializeApp(firebaseConfig);
+
+  export default fire;
+ 
