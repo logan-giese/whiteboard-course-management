c9c94422679979510fb92af32ce4cb10d54a6aaa
gopiyerabati95 <g.yerabati@eagles.oc.edu>
2021-07-06 18:34:13 -0500

    cards

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,8 +1,35 @@
 import React, {Component} from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 class Tools extends Component{
     render(){
         return (
-        <h1>Tools page</h1>
+        
+            <div>
+                <h1>Tools page</h1>
+             <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Content Collection</Card.Title>
+              <Card.Text>
+                All the contents of the student.
+              </Card.Text>
+              <Button variant="primary">Contents</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Portfolios</Card.Title>
+              <Card.Text>
+                All the portfolios of the student.
+              </Card.Text>
+              <Button variant="primary">Portfolios</Button>
+            </Card.Body>
+          </Card>
+          </div>
+        
+      
         );
     }
     
