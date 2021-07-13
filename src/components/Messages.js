4fac29366fd24d43358e8f399122b7415f7ea2a1
gopiyerabati95 <g.yerabati@eagles.oc.edu>
2021-07-13 18:46:15 -0500

    navigation from messages to inbox

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
+import {Link } from "react-router-dom";
+
 
 function About() {
     return <div className="content">
         <h1>Messages page</h1>
-        <Button> Inbox </Button> 
+        <Link to="/inbox"><Button>
+              Inbox 
+            </Button>
+            </Link> 
         <Button> Sent </Button>
       </div>;
   }
